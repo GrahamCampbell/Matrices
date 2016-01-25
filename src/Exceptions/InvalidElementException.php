08d4404c0ff7829b7660963c684c5b76d194d6dc
Graham Campbell <graham@alt-three.com>
2016-01-25 22:32:24 +0000

    Added a way to access the individual elements

---
+++ src/Exceptions/InvalidElementException.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of Matrices.
+ *
+ * (c) Graham Campbell <graham@alt-three.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace GrahamCampbell\Matrices\Exceptions;
+
+use InvalidArgumentException;
+
+/**
+ * This is the invalid element exception class.
+ *
+ * @author Graham Campbell <graham@alt-three.com>
+ */
+class InvalidElementException extends InvalidArgumentException implements MatricesExceptionInterface
+{
+    //
+}
